a140d8444eab1cc686da39535797f85acbc17abb
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-06-07 11:50:24 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(),nums.end());
+        if(nums.size()==s.size()){
+            return false;
+        }
+        else return true;
+    }
+};
