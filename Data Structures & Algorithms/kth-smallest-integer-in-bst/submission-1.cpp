db46d880eb26288724edc6b78bf3dcd73e400f36
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-09-07 20:06:48 +0530

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> tmp(2);
+        tmp[0] = k;
+        dfs(root, tmp);
+        return tmp[1];
+    }
+    void dfs(TreeNode* node, vector<int>& tmp) {
+        if(!node) return;
+        dfs(node->left, tmp);
+        if(tmp[0] == 0) return;
+        tmp[0]--;
+        if(tmp[0] == 0){
+            tmp[1] = node->val;
+            return;
+        }
+        dfs(node->right, tmp);
+    }
+};
