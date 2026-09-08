df494e6a995159517fc8beb62a85140abed69cb5
Saumitra Kulkarni <147703962+Saumi18@users.noreply.github.com>
2026-09-08 23:20:48 +0530

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+public:
+    void serializeDFS(TreeNode* root, string& s) {
+        if (!root) {
+            s += "#,";
+            return;
+        }
+
+        s += to_string(root->val) + ",";
+        serializeDFS(root->left, s);
+        serializeDFS(root->right, s);
+    }
+
+    string serialize(TreeNode* root) {
+        string s;
+        serializeDFS(root, s);
+        return s;
+    }
+
+    TreeNode* deserializeDFS(stringstream& ss) {
+        string val;
+        getline(ss, val, ',');
+
+        if (val == "#")
+            return nullptr;
+
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserializeDFS(ss);
+        root->right = deserializeDFS(ss);
+
+        return root;
+    }
+
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return deserializeDFS(ss);
+    }
+};
